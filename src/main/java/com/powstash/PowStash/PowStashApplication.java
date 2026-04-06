15159d77296e8a9bb136cd27d23973bebfd9e280
Paul Calabrese <pacalabre@gmail.com>
2026-04-06 17:38:22 -0600

    Changing lombok to provided

--- src/main/java/com/powstash/PowStash/PowStashApplication.java
+++ src/main/java/com/powstash/PowStash/PowStashApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PowStashApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(PowStashApplication.class, args);
 	}
 
