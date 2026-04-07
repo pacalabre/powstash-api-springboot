e36bcdf977ed3e14a0fe81bb4273e24f3b240c34
Paul Calabrese <pacalabre@gmail.com>
2026-04-07 15:11:32 -0600

    Adding CORS configuration

--- src/main/java/com/powstash/PowStash/PowStashApplication.java
+++ src/main/java/com/powstash/PowStash/PowStashApplication.java
@@ -1,7 +1,11 @@
 package com.powstash.PowStash;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PowStashApplication {
@@ -10,4 +14,20 @@ public class PowStashApplication {
 		SpringApplication.run(PowStashApplication.class, args);
 	}
 
+
+    @Configuration
+    public class CorsConfig implements WebMvcConfigurer {
+        @Value("${spring.application.allowed-origin}")
+        private String allowedOrigin;
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins(allowedOrigin)
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
+
+    }
 }
